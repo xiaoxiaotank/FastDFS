4e1360bbed7b528db5d617f594a3b35d832055ac
xiaoxiao <jiajianj2057@qq.com>
2020-02-18 19:05:37 +0800

    init

---
+++ FastDFS.Client.4.5/FastDFS.Client.4.5/MyFastDFSClient.cs
@@ -0,0 +1,36 @@
+﻿using FastDFS.Client.Common;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDFS.Client._4._5
+{
+    public class MyFastDFSClient : FastDFSClient
+    {
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="node">存储节点</param>
+        /// <param name="filePath">文件路径</param>
+        /// <return>远程文件Url</return>
+        public static string UploadFile(StorageNode node, string filePath)
+        {
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+            if (File.Exists(filePath))
+            {
+                var fileBytes = File.ReadAllBytes(filePath);
+                var fileUrl = UploadFile(node, fileBytes, Path.GetExtension(filePath).TrimStart('.'));
+                return fileUrl;
+            }
+            else
+            {
+                throw new ArgumentException("文件不存在！");
+            }
+        }
+    }
+}
---
+++ FastDFS.Client.4.5/FastDFS.Client.4.5/Program.cs
@@ -0,0 +1,106 @@
+﻿using FastDFS.Client.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDFS.Client._4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 初始化
+            // 所有 tracker 服务器的ip:port
+            var trackerIPs = new List<IPEndPoint>()
+            {
+                new IPEndPoint(IPAddress.Parse("192.168.181.128"), 22122)
+            };
+            if (!ConnectionManager.Initialize(trackerIPs))
+            {
+                throw new Exception("fastDFS连接初始化失败！");
+            }
+
+            var node = FastDFSClient.GetStorageNode("group1");
+            #endregion
+
+            #region 小文件上传
+            //var uploadingfilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\1.jpg");
+            //var uploadedfileUrl1 = MyFastDFSClient.UploadFile(node, uploadingfilePath);
+            //Console.WriteLine($"小文件已上传，Url 为：{uploadedfileUrl1}");
+
+
+            //var slaveFileName = FastDFSClient.UploadSlaveFile("group1", content, fileName, "-part1", "jpg");
+            #endregion
+
+            #region 大文件上传
+
+
+
+            #endregion
+
+            #region 文件查询
+            var queryFileUrl = "M00/00/00/wKi1gF5LsuiAdeRbAAYD2YvCnl8857.jpg";
+            try
+            {
+                var fileInfo = FastDFSClient.GetFileInfo(node, queryFileUrl);
+                Console.WriteLine($"文件大小：{fileInfo.FileSize}");
+                Console.WriteLine($"文件创建时间：{fileInfo.CreateTime.AddHours(8):yyyy-MM-dd HH:mm:ss}");
+            }
+            catch (FDFSException ex)
+            {
+                Console.WriteLine($"文件不存在：{queryFileUrl}");
+                Console.WriteLine($"Message：{ex.Message}");
+            }           
+           
+            #endregion
+
+            #region 小文件下载        
+            var downloadingfileUrl = "M00/00/00//wKi1gF5LsuiAdeRbAAYD2YvCnl8857.jpg";
+            var localPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $@"..\..\download\1{Path.GetExtension(downloadingfileUrl)}");
+
+            // 出现图片内容丢失
+            //var downloadingfileBytes = FastDFSClient.DownloadFile(node, downloadingfileUrl);
+            //File.WriteAllBytes(localPath, downloadingfileBytes);
+
+            //var downloadingfileUrl = $"http://192.168.181.128:8888/group1/{downloadingfileUrl}";
+            //using (WebClient web = new WebClient())
+            //{
+            //    Directory.CreateDirectory(Path.GetDirectoryName(localPath));
+            //    web.DownloadFile(downloadingfileUrl, localPath);
+
+            //    //Console.WriteLine("文件已下载完毕，稍后将自动打开。。。");
+            //    //Process.Start("explore.exe", localPath);
+            //}
+
+            #endregion
+
+            #region 大文件下载
+
+            #endregion
+
+            #region 文件删除
+            var removingfileName = "M00/00/00/wKi1gF5LsuiAdeRbAAYD2YvCnl8857.jpg";
+            try
+            {
+                FastDFSClient.RemoveFile(node.GroupName, removingfileName);
+                Console.WriteLine($"文件已删除：{removingfileName}");
+            }
+            catch(FDFSException ex)
+            {
+                Console.WriteLine($"文件不存在：{queryFileUrl}");
+                Console.WriteLine($"Message：{ex.Message}");
+            }
+            #endregion
+
+            Console.WriteLine("ending........................");
+            Console.ReadKey();
+        }
+
+
+    }
+}
